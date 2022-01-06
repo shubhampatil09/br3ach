4ff496a0e645fc3ba64af2e3a2ac661c0e970446
shubham patil <shubhampatil151@gmail.com>
2022-01-06 15:58:17 +0530

    Update br3ach.py

--- br3ach.py
+++ br3ach.py
@@ -15,7 +15,6 @@ if ( a < b ):
  #z = ('"' + str(x) + '"')
  y.write(z)
  y.close()
- import config 
  file1 = open("config.txt","r+") 
  M = (file1.read())
  val = input("Enter your username,email or phone: ")
@@ -27,8 +26,7 @@ if ( a < b ):
 
  response = requests.request("GET", url, headers=headers, params=querystring)
  print(response.text)
-else : 
- import config 
+else :  
  file1 = open("config.txt","r+") 
  M = (file1.read())
  val = input("Enter your username,email or phone: ")
