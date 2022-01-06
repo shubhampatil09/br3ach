b9636597a89919c5344dfcafc83d4d4c6238b7b8
shubham patil <shubhampatil151@gmail.com>
2022-01-06 16:25:21 +0530

    Update br3ach.py

--- br3ach.py
+++ br3ach.py
@@ -5,18 +5,18 @@ url = "https://breachdirectory.p.rapidapi.com/"
 import os
 
 # get the size of file
-a = os.path.getsize('config.py')
-b = 1
+a = os.path.getsize('config.txt')
+b = 3
 if ( a < b ):
  x = input("Api_Key : ")
- y = open('config.py', 'w')
+ y = open('config.txt', 'w')
  #z = ('username ='' "' + str(x) + '"')
  z= (x)
  #z = ('"' + str(x) + '"')
  y.write(z)
  y.close()
  import config 
- file1 = open("config.py","r+") 
+ file1 = open("config.txt","r+") 
  M = (file1.read())
  val = input("Enter your username,email or phone: ")
  headers = {
@@ -29,7 +29,7 @@ if ( a < b ):
  print(response.text)
 else : 
  import config 
- file1 = open("config.py","r+") 
+ file1 = open("config.txt","r+") 
  M = (file1.read())
  val = input("Enter your username,email or phone: ")
  headers = {
